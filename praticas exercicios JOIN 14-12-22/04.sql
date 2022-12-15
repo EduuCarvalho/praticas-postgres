c72f74cd75c260be233a553b37cf004360e7a8d4
EduuCarvalho <educarvalho.engcivil@gmail.com>
2022-12-14 21:03:46 -0300

    pratica JOIN

---
+++ praticas exercicios JOIN 14-12-22/04.sql
@@ -0,0 +1,10 @@
+SELECT users.id, users.name, roles.name AS "role", companies.name AS company, experiences."startDate" FROM users JOIN experiences ON users.id = experiences."userId"
+JOIN roles ON experiences."roleId" = roles.id
+JOIN companies ON experiences."companyId" = companies.id
+WHERE users.id = 50
+AND experiences."endDate" IS NULL;
+
+|---id---|---name---|--------role--------|----company----|---startDate---|
+    1        João      Software Engineer        OLX          2020-06-01
+    2        João         Consultant            IBM          2022-02-01
+   ...        ...            ...                ...              ...\q
